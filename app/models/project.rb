61b7d31305ca04ebafb73b492916fd0da921168c
tiago <tiago.ameller@gmail.com>
2015-02-02 15:25:16 +0100

    another method to project.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,11 @@
 class Project < ActiveRecord::Base
 	# for change table name (optional)
 	# set table_name = "projects"
+	def self.iron_find(n)
+		where("name = ?",n).first
+	end
+
+	def self.clean_old
+		where("created_at < ?",7.day.ago).destroy_all
+	end
 end
