dcc1341a1128e5bfe78537d3fb984ac6999021d2
tiago <tiago.ameller@gmail.com>
2015-02-02 11:00:21 +0100

    first to views working

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,13 @@
+class SiteController < ApplicationController
+	# auto created with "rails g controller site"
+	# actions will be methods here
+	# if controller name == views folder name this is not necessary
+=begin
+	def home
+		render 'home'
+	end
+	def contact
+		render 'contact'
+	end
+=end
+end
